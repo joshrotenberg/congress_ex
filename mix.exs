9fc3b12bd53467fbef7bdf14283f54e721feebee
josh rotenberg <joshrotenberg@gmail.com>
2023-06-20 10:11:31 -0700

    format

--- mix.exs
+++ mix.exs
@@ -9,8 +9,11 @@ defmodule Congress.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       preferred_cli_env: [
-        vcr: :test, "vcr.delete": :test, "vcr.check": :test, "vcr.show": :test
-      ],
+        vcr: :test,
+        "vcr.delete": :test,
+        "vcr.check": :test,
+        "vcr.show": :test
+      ]
     ]
   end
 
