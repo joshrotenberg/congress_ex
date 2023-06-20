727b04ae86218d021e18608bd38c3ac65122f284
josh rotenberg <joshrotenberg@users.noreply.github.com>
2023-06-20 16:10:42 -0700

    soemthing is better than nothing (#3)

--- lib/congress.ex
+++ lib/congress.ex
@@ -9,6 +9,14 @@ defmodule Congress do
   @default_user_agent "Congress (elixir)/#{Mix.Project.config()[:version]}"
   @default_api_key_env "CONGRESS_API_KEY"
 
+  @doc """
+  Creates a new `Congress` client reference, optionally specifying several parameters.
+
+   * :api_key - your API key. uses @default_api_key_env from the environment by default
+   * :base_url -  the base url for the API. default: @default_base_url
+   * :user_agent - the User-Agent HTTP header value. default: @default_user_agent
+
+  """
   def new(opts \\ []) do
     api_key = Keyword.get(opts, :api_key, System.get_env(@default_api_key_env))
     base_url = Keyword.get(opts, :base_url, @default_base_url)
@@ -24,12 +32,21 @@ defmodule Congress do
     )
   end
 
+  @doc """
+  Return the previous page of items, if any. Takes the client reference and a response from 
+  any API call that returns a list of items.
+  """
   def previous(req, %{pagination: %{prev: prev}}) when prev != nil, do: page(req, prev)
   def previous(_req, _resp), do: {:error, "No previous element"}
 
+  @doc """
+  Return the next page of items, if any. Takes the client reference and a response from 
+  any API call that returns a list of items.
+  """
   def next(req, %{pagination: %{next: next}}) when next != nil, do: page(req, next)
   def next(_req, _resp), do: {:error, "No next element"}
 
+  @doc false
   defp page(req, url) do
     uri = url |> URI.parse()
     query = URI.decode_query(uri.query) |> Map.to_list()
--- lib/congress/bill.ex
+++ lib/congress/bill.ex
@@ -3,6 +3,16 @@ defmodule Congress.Bill do
   `Congress.Bill`: fetch bills and bill information.
   """
 
+  @doc """
+  Returns a list of bills sorted by date of latest action.
+
+  Optional parameters allow finer control:
+
+   * :congress - limit results to bills in the specified Congress
+   * :bill_type - limit results to bills of the specified type
+
+  Note that `:bill_type` requires `:congress`.
+  """
   def bills(req, opts \\ []) do
     params = Congress.opts_helper(opts)
 
@@ -14,33 +24,63 @@ defmodule Congress.Bill do
     end
   end
 
+  @doc """
+  Returns detailed information for a specified bill.
+  """
   def bill(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number], opts)
 
+  @doc """
+  Returns the list of actions on a specified bill.
+  """
   def actions(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number, "actions"], opts)
 
+  @doc """
+  Returns the list of amendments to a specified bill.
+  """
   def amendments(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number, "amendments"], opts)
 
+  @doc """
+  Returns the list of committees associated with a specified bill.
+  """
   def committees(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number, "committees"], opts)
 
+  @doc """
+  Returns the list of cosponsors on a specified bill.
+  """
   def cosponsors(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number, "cosponsors"], opts)
 
+  @doc """
+  Returns the list of related bills to a specified bill.
+  """
   def related_bills(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number, "relatedbills"], opts)
 
+  @doc """
+  Returns the list of legislative subjects on a specified bill.
+  """
   def subjects(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number, "subjects"], opts)
 
+  @doc """
+  Returns the list of summaries for a specified bill.
+  """
   def summaries(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number, "summaries"], opts)
 
+  @doc """
+  Returns the list of text versions for a specified bill.
+  """
   def text(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number, "text"], opts)
 
+  @doc """
+  Returns the list of titles for a specified bill.
+  """
   def titles(req, congress, bill_type, bill_number, opts \\ []),
     do: bill_request(req, [congress, bill_type, bill_number, "titles"], opts)
 
