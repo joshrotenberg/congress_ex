6ada5482cbbbcfdcd38c0454c1c53a82efc6489b
josh rotenberg <joshrotenberg@gmail.com>
2023-07-07 15:09:21 -0700

    lazy

--- test/congress/exvcr/bill_test.exs
+++ test/congress/exvcr/bill_test.exs
@@ -33,4 +33,56 @@ defmodule Congress.ExVCR.BillTest do
       end
     end
   end
+
+  describe "bill" do
+    test "get a bill", %{req: req} do
+      {:ok, response} = Congress.Bill.bill(req, 117, :hr, 3076)
+      assert response.bill != nil
+    end
+
+    test "get a bill's actions", %{req: req} do
+      {:ok, response} = Congress.Bill.actions(req, 117, :hr, 3076)
+      assert response.actions != nil
+    end
+
+    test "get a bill's amendments", %{req: req} do
+      {:ok, response} = Congress.Bill.amendments(req, 117, :hr, 3076)
+      assert response.amendments != nil
+    end
+
+    test "get a bill's committees", %{req: req} do
+      {:ok, response} = Congress.Bill.committees(req, 117, :hr, 3076)
+      assert response.committees != nil
+    end
+
+    test "get a bill's cosponsors", %{req: req} do
+      {:ok, response} = Congress.Bill.cosponsors(req, 117, :hr, 3076)
+      assert response.cosponsors != nil
+    end
+
+    test "get a bill's related bills", %{req: req} do
+      {:ok, response} = Congress.Bill.related_bills(req, 117, :hr, 3076)
+      assert response.related_bills != nil
+    end
+
+    test "get a bill's subjects", %{req: req} do
+      {:ok, response} = Congress.Bill.subjects(req, 117, :hr, 3076)
+      assert response.subjects != nil
+    end
+
+    test "get a bill's summaries", %{req: req} do
+      {:ok, response} = Congress.Bill.summaries(req, 117, :hr, 3076)
+      assert response.summaries != nil
+    end
+
+    test "get a bill's text", %{req: req} do
+      {:ok, response} = Congress.Bill.text(req, 117, :hr, 3076)
+      assert response.text_versions != nil
+    end
+
+    test "get a bill's titles", %{req: req} do
+      {:ok, response} = Congress.Bill.titles(req, 117, :hr, 3076)
+      assert response.titles != nil
+    end
+  end
 end
